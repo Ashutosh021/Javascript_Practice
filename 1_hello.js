95ff0c469c27fb820be2e18f51a79729568c2c2c
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2024-01-01 22:50:47 +0530

    Add files via upload

---
+++ 1_hello.js
@@ -0,0 +1,4 @@
+console.log("Hello World !");
+
+var greet = "Again Hello World";
+console.log(greet);
---
+++ 2_Add.js
@@ -0,0 +1,8 @@
+var a=12;
+var b=14;
+console.log("string concat :- "+a+b);
+console.log("sum :- "+(a+b));
+
+var sum = a+b;
+console.log("sum :- "+sum);
+
