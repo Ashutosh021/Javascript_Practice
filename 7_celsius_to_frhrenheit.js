661ba3c29added0c42c61c19a42425646cfdcad4
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2024-01-04 22:07:06 +0530

    Add files via upload

---
+++ 7_celsius_to_frhrenheit.js
@@ -0,0 +1,8 @@
+// celcius into farenheit          °F = (9/5 × °C) + 32
+// farenheit into celcius          °C = (°F - 32) * 5/9.
+
+var C = 12;
+var F = 42;
+
+console.log(`${C} C = ${((9/5 * C)+32)} F`);
+console.log(`${F} C = ${((F - 32)*5/9)} F`);
