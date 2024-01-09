92ea9529983993e68997bd0e0d90e011795b6507
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2024-01-09 20:36:16 +0530

    Add files via upload

---
+++ 11_check_posi_negi_zero.js
@@ -0,0 +1,11 @@
+var num = -2;
+
+if(num > 0){
+    console.log(`${num} is Positive`);
+}
+else if(num<0){
+    console.log(`${num} is Negetive`);
+}
+else{
+    console.log("ZERO");
+}
---
+++ 12_even_odd.js
@@ -0,0 +1,8 @@
+var num = 13;
+
+if(num%2==0){
+    console.log(`${num} is EVEN`);
+}
+else{
+    console.log(`${num} is ODD`);
+}
