f3c384c09a1d923eb8b42b9f5bf786bd2cbcf4a7
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2024-01-21 22:48:07 +0530

    Add files via upload

---
+++ 33_find_ASCII.js
@@ -0,0 +1,6 @@
+function findASCII(ch){
+    console.log(ch.charCodeAt(0));
+}
+findASCII('A');
+findASCII('B');
+findASCII('C');
---
+++ 34_set_default_para.js
@@ -0,0 +1,7 @@
+function abc(num=56){
+    console.log(num);
+}
+
+
+abc();   //print default
+abc(13); //override default
