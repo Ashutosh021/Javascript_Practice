6ca724d04f546a7ba26c757aff5e43d848fbdce4
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2024-01-23 22:47:33 +0530

    Add files via upload

---
+++ 37_function_overloading.js
@@ -0,0 +1,10 @@
+function abc(a,b){
+    console.log(a+b);
+}
+
+function abc(c,d){
+    console.log(c+d);
+}
+
+abc(5,6);
+abc(5.5,7.7);
---
+++ 38_pass_func_para.js
@@ -0,0 +1,9 @@
+var a = function abc(){
+    console.log("Hii This is abc function");
+}
+
+function printit(fnc){
+    fnc();
+}
+
+printit(a);
