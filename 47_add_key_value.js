d623428578562cbdee627ef8d1151f8b35fbe502
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2024-01-29 20:56:58 +0530

    Add files via upload

---
+++ 47_add_key_value.js
@@ -0,0 +1,11 @@
+var student = {
+    name: "Ashutosh",
+    age: 21,
+    roll_no: 10,
+    subject: ["COA", "DM", "FCET", "C", "POMC"],
+  };
+  
+
+  console.log(student);
+  student.marks = 320;
+  console.log(student);
---
+++ 48_convert_str.js
@@ -0,0 +1,15 @@
+var student = {
+  name: "Ashutosh",
+  age: 21,
+  roll_no: 10,
+  subject: ["COA", "DM", "FCET", "C", "POMC"],
+};
+console.log(student);
+
+var res = JSON.stringify(student);
+console.log(res);
+console.log(typeof res);
+
+
+
+
