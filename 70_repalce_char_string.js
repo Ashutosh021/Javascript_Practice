83e3538f16b1aedcdccaf6ec49166afce1b2cb8e
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2024-02-16 21:11:23 +0530

    Add files via upload

---
+++ 70_repalce_char_string.js
@@ -0,0 +1,6 @@
+function replaceWord(str ,replaceThis , replaceBy){
+    const ans = str.replace(replaceThis , replaceBy);
+    console.log(ans);
+}
+
+replaceWord("This is a string","This","YES!");
